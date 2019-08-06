e824f173f5da306dd6ff6a33ee54d1bd9c49cd7f
bashokusan <pierresimonnet1@gmail.com>
2019-08-06 11:21:52 +0200

    refactoring router

--- Public/index.php
+++ Public/index.php
@@ -13,38 +13,54 @@ use App\Database;
 use App\Helpers;
 use App\model\PostManager;
 
-
 // Routeur
 ob_start();
 
-if($_GET['page'] === 'home' || !$_GET || empty($_GET['page']))
+// Vérifie si on est sur la racine
+if($_SERVER['REQUEST_URI'] === '/P5/Public/index.php')
 {
   require_once "../Views/home.php";
 }
 
-elseif($_GET['page'] === 'blog')
+// Vérifie si il y a un paramètre page
+elseif(isset($_GET['page']))
 {
-  $response = PostManager::showAll();
+  $page = htmlentities($_GET['page']);
 
-  require_once "../Views/blog.php";
-}
-
-elseif($_GET['page'] == 'post' && isset($_GET['id']))
-{
-  if((int)$_GET['id'] <= (int)PostManager::count()[0]){
-    $response = PostManager::showOne($_GET['id']);
-
-    require_once "../Views/post.php";
+  if($page === 'home')
+  {
+    require_once "../Views/home.php";
   }
-  else{
-    require_once "../Views/notFound.php";
+
+  elseif($page === 'blog')
+  {
+    $response = PostManager::showAll();
+    require_once "../Views/blog.php";
+  }
+
+  elseif($page == 'post' && isset($_GET['id']))
+  {
+    // Vérifie si l'id de l'url est inférieur ou égal au nombre d'article en BDD
+    if(((int)$_GET['id']) &&  (int)$_GET['id'] <= (int)PostManager::count()[0])
+    {
+      $response = PostManager::showOne($_GET['id']);
+      require_once "../Views/post.php";
+    }
+    else
+    {
+      echo "id invalide";
+    }
+  }
+  else
+  {
+    echo "page introuvable";
   }
 
 }
 
 else
 {
-  require_once "../Views/notFound.php";
+  echo "cette page n'existe pas";
 }
 
 $content = ob_get_clean();
